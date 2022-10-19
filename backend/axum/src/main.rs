f73b3a83282c5c3e031a48c40a4a201ac073e192
2814109 <2814109k@gmail.com>
2022-10-20 07:28:16 +0900

    add routing

--- backend/axum/src/main.rs
+++ backend/axum/src/main.rs
@@ -6,7 +6,7 @@ use axum::{
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(|| async { "Hello, World!" })).route("/test", get(|| async { "Hello, World! 2" }));
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3333".parse().unwrap())
