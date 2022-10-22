ffbb1544c06d367e3f35d9c234bbc41f8bf03b49
2814109 <2814109k@gmail.com>
2022-10-22 23:03:28 +0900

    demo customRepository

--- backend/axum/src/infrastructure/repository/task.rs
+++ backend/axum/src/infrastructure/repository/task.rs
@@ -1,1 +1,11 @@
+use crate::entity::prelude::Tasks;
 
+// Custom する関数を定義
+trait CustomTaskRepository {
+    fn test(&mut self);
+}
+
+// implでtraitをstructに注入
+impl CustomTaskRepository for Tasks {
+    fn test(&mut self) {}
+}
