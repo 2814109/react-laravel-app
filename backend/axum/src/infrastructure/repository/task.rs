438a6702e3e0e7a10f3b9d297bc234155f226556
2814109 <2814109k@gmail.com>
2022-10-23 00:43:54 +0900

    update custom resopitory

--- backend/axum/src/infrastructure/repository/task.rs
+++ backend/axum/src/infrastructure/repository/task.rs
@@ -1,11 +1,16 @@
 use crate::entity::prelude::Tasks;
 
+// create時の引数を明示的に型定義する
+struct CreateTask {
+    title: String,
+    is_closed: bool,
+}
 // Custom する関数を定義
 trait CustomTaskRepository {
-    fn test(&mut self);
+    fn create_one(&mut self);
 }
 
 // implでtraitをstructに注入
 impl CustomTaskRepository for Tasks {
-    fn test(&mut self) {}
+    fn create_one(&mut self) {}
 }
