cc47351c3c47b26895f6842eeecbe34acbcbbd89
2814109 <2814109k@gmail.com>
2022-10-23 01:04:39 +0900

    create repository

--- backend/axum/src/infrastructure/repository/task.rs
+++ backend/axum/src/infrastructure/repository/task.rs
@@ -7,10 +7,12 @@ struct CreateTask {
 }
 // Custom する関数を定義
 trait CustomTaskRepository {
-    fn create_one(&mut self);
+    fn create_one(create_task: CreateTask);
 }
 
 // implでtraitをstructに注入
 impl CustomTaskRepository for Tasks {
-    fn create_one(&mut self) {}
+    fn create_one(mut create_task: CreateTask) {
+        // entity を使ってinsert処理を実行
+    }
 }
