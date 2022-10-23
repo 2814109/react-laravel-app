85df3b1c57ff66dcf3f9fa6daa00d1eabfcc6109
2814109 <2814109k@gmail.com>
2022-10-23 16:48:19 +0900

    join controller and repository

--- backend/axum/src/main.rs
+++ backend/axum/src/main.rs
@@ -4,12 +4,12 @@ use crate::presentation::controller::router;
 pub mod entity;
 pub mod infrastructure;
 pub mod presentation;
-use crate::infrastructure::repository::task;
+// use crate::infrastructure::repository::task;
 // mod task;
 mod work;
 #[tokio::main]
 async fn main() {
-    task::create_one("test3").await;
+    // task::create_one("test3").await;
     //  let db: DatabaseConnection =
     Database::connect("postgresql://postgres:postgres@localhost:5432/postgres".to_string())
         .await
--- backend/axum/src/presentation/controller/router.rs
+++ backend/axum/src/presentation/controller/router.rs
@@ -1,8 +1,10 @@
+use crate::infrastructure::repository::task;
 use axum::{routing::get, Router};
-
 pub fn app() -> Router {
     let router: Router = Router::new()
         .route("/", get(|| async { "Hello, World! 1" }))
-        .route("/test", get(|| async { "Hello, World! 2" }));
+        .route("/test", get(|| async { "Hello, World! 2" }))
+        .route("/response", get(|| async { task::create_one("api").await }));
+
     return router;
 }
