9c0751be853f56bcc759f9a1de75736f80925470
2814109 <2814109k@gmail.com>
2022-10-23 17:23:35 +0900

    find by id

--- backend/axum/src/infrastructure/repository/task.rs
+++ backend/axum/src/infrastructure/repository/task.rs
@@ -1,6 +1,13 @@
+use crate::entity::prelude::Tasks;
 use crate::entity::tasks;
+// use axum::response;
 use chrono::{DateTime, Utc};
-use sea_orm::{ActiveModelTrait, ActiveValue, Database, DatabaseConnection};
+use sea_orm::{ActiveModelTrait, ActiveValue, Database, DatabaseConnection, EntityTrait};
+use serde::Serialize;
+#[derive(Serialize)]
+struct Task {
+    id: i32,
+}
 
 pub async fn create_one(title: &str) {
     let utc: DateTime<Utc> = Utc::now();
@@ -19,4 +26,16 @@ pub async fn create_one(title: &str) {
 
     let result = new_task.insert(&db).await;
     println!("{:?}", result);
+}
+
+pub async fn get_task_by_id() {
+    let db: DatabaseConnection =
+        Database::connect("postgresql://postgres:postgres@localhost:5432/postgres".to_string())
+            .await
+            .expect("Database connection failed");
+
+    let result = Tasks::find_by_id(5).one(&db).await;
+    println!("{:?}", result);
+
+    // return response::Json(Task { id: 11 });
 }
--- backend/axum/src/presentation/controller/router.rs
+++ backend/axum/src/presentation/controller/router.rs
@@ -1,10 +1,11 @@
 use crate::infrastructure::repository::task;
+// use axum::response::Json;
 use axum::{routing::get, Router};
 pub fn app() -> Router {
     let router: Router = Router::new()
         .route("/", get(|| async { "Hello, World! 1" }))
         .route("/test", get(|| async { "Hello, World! 2" }))
-        .route("/response", get(|| async { task::create_one("api").await }));
-
+        .route("/response", get(|| async { task::create_one("api").await }))
+        .route("/task", get(|| async { task::get_task_by_id().await }));
     return router;
 }
