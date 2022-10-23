1be6cab75a91c65e41ea0bed7ed3742b43753943
2814109 <2814109k@gmail.com>
2022-10-23 13:42:02 +0900

    add runtime setting

--- backend/axum/src/infrastructure/repository/task.rs
+++ backend/axum/src/infrastructure/repository/task.rs
@@ -1,5 +1,6 @@
-use crate::entity::prelude::Tasks;
+use sea_orm::{ActiveModelTrait, ActiveValue, Database, DatabaseConnection};
 
+use crate::entity::{prelude::Tasks, tasks};
 // create時の引数を明示的に型定義する
 struct CreateTask {
     title: String,
@@ -14,5 +15,14 @@ trait CustomTaskRepository {
 impl CustomTaskRepository for Tasks {
     fn create_one(mut create_task: CreateTask) {
         // entity を使ってinsert処理を実行
+        let new_task = tasks::ActiveModel {
+            id: ActiveValue::NotSet,
+            title: ActiveValue::Set("test".to_owned()),
+            is_closed: ActiveValue::Set(false),
+            created_at: ActiveValue::NotSet,
+            updated_at: ActiveValue::NotSet,
+        };
+
+        // let new_task: tasks::Model = new_task.insert().await?;
     }
 }
--- backend/axum/src/main.rs
+++ backend/axum/src/main.rs
@@ -1,3 +1,5 @@
+use sea_orm::{Database, DatabaseConnection};
+
 use crate::presentation::controller::router;
 pub mod entity;
 pub mod infrastructure;
@@ -5,6 +7,10 @@ pub mod presentation;
 mod work;
 #[tokio::main]
 async fn main() {
+    Database::connect("postgresql://postgres:postgres@localhost:5432/postgres".to_string())
+        .await
+        .expect("Database connection failed");
+
     work::debug();
     axum::Server::bind(&"0.0.0.0:3333".parse().unwrap())
         .serve(router::app().into_make_service())
