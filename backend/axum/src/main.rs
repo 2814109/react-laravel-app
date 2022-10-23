4cd91787bb1e6b769f86fd448f9432490f908ed2
2814109 <2814109k@gmail.com>
2022-10-23 14:36:45 +0900

    insert data

--- backend/axum/src/main.rs
+++ backend/axum/src/main.rs
@@ -1,12 +1,14 @@
-use sea_orm::{Database, DatabaseConnection};
+use sea_orm::Database;
 
 use crate::presentation::controller::router;
 pub mod entity;
-pub mod infrastructure;
 pub mod presentation;
+mod task;
 mod work;
 #[tokio::main]
 async fn main() {
+    task::create_one().await;
+    //  let db: DatabaseConnection =
     Database::connect("postgresql://postgres:postgres@localhost:5432/postgres".to_string())
         .await
         .expect("Database connection failed");
